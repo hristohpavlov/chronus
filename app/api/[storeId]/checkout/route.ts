1d0a2beb3091031eb51527c42a610d40416ea293
Hristo  Pavlov <hristohpavlov@gmail.com>
2024-02-13 00:40:15 +0200

    [Feature] Add orders entity and payment with stripe

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,70 +1,83 @@
-import prismadb from "@/lib/prismadb";
-import { auth } from "@clerk/nextjs";
+import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request, { params} : { params: {storeId: string}}) {
-    try {
-        const { userId } = auth();
-        const  body = await req.json();
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
 
-        const { label, imageUrl } = body;
-        if(!userId){
-            return new NextResponse("Unauthenticated", {status: 401});
-        }
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
-        if(!label){
-            return new NextResponse("Label is required", {status: 400});
-        }
-
-        if(!imageUrl){
-            return new NextResponse("Image URL is required", {status: 400});
-        }
-
-        if(!params.storeId){
-            return new NextResponse("storeId is required", {status: 400});
-        }
-
-        const storeByUserId = await prismadb.store.findFirst({
-            where: {
-                id: params.storeId,
-                userId
-            }
-        })
-
-        if(!storeByUserId){
-            return new NextResponse("Unauthorized", {status: 403});
-        }
-
-        const billboard = await prismadb.billboard.create({
-            data: {
-                label,
-                imageUrl,
-                storeId: params.storeId
-            }
-        });
-
-        return NextResponse.json(billboard);
-    } catch (error){
-        console.log("[BILLBOARDS_POST]", error);
-        return new NextResponse("Internal error", {status: 500});
-    }
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
 }
 
-export async function GET(req: Request, { params} : { params: {storeId: string}}) {
-    try {
-        if(!params.storeId){
-            return new NextResponse("storeId is required", {status: 400});
-        }
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  const { productIds } = await req.json();
 
-        const billboards = await prismadb.billboard.findMany({
-            where: {
-                storeId: params.storeId
-            }
-        });
+  if (!productIds || productIds.length === 0) {
+    return new NextResponse("Product ids are required", { status: 400 });
+  }
 
-        return NextResponse.json(billboards);
-    } catch (error){
-        console.log("[BILLBOARDS_GET]", error);
-        return new NextResponse("Internal error", {status: 500});
+  const products = await prismadb.product.findMany({
+    where: {
+      id: {
+        in: productIds
+      }
     }
-}
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+  products.forEach((product) => {
+    line_items.push({
+      quantity: 1,
+      price_data: {
+        currency: 'USD',
+        product_data: {
+          name: product.name,
+        },
+        unit_amount: product.price.toNumber() * 100
+      }
+    });
+  });
+
+  const order = await prismadb.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: false,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId
+            }
+          }
+        }))
+      }
+    }
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: 'payment',
+    billing_address_collection: 'required',
+    phone_number_collection: {
+      enabled: true,
+    },
+    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+    metadata: {
+      orderId: order.id
+    },
+  });
+
+  return NextResponse.json({ url: session.url }, {
+    headers: corsHeaders
+  });
+};
